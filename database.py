665e41d9989bf4b6276e399cd12ebad8c0ec6162
Igor Voskoboinikov <voskoboinikov777@gmail.com>
2024-10-06 12:30:02 +0300

    updated data from settings

--- database.py
+++ database.py
@@ -2,12 +2,9 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-import config
+from settings import settings
 
-SQLALCHEMY_DATABASE_URL = f"postgresql+asyncpg://{config.POSTGRES_USER}:{config.POSTGRES_PASSWORD}" \
-                          f"@{config.POSTGRES_HOST}:{config.POSTGRES_PORT}/{config.POSTGRES_DB}"
-
-engine = create_async_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_async_engine(settings.SQLALCHEMY_DATABASE_URL)
 
 SessionLocal: sessionmaker[AsyncSession] = sessionmaker[AsyncSession](
     autocommit=False,
